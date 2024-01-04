84b80b3b1a03377e6a7dbdb41effaf8addb61786
Guilherme Araujo <99432207+GuilhermeAraujo062@users.noreply.github.com>
2024-01-04 14:07:33 -0300

    add

---
+++ seçao3/exercicio-logica/logica01.js
@@ -0,0 +1,9 @@
+// function max(x, y) {
+//     // if (x > y) return x;
+//     // return y;
+//     return x > y ? x : y
+// }
+
+const max2 = (x, y) => x > y ? x : y;
+
+console.log(max2(12, 100));
