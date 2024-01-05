26236e0da5abde742aaee0ef097c95ba9c117a1b
Guilherme Araujo <99432207+GuilhermeAraujo062@users.noreply.github.com>
2024-01-05 12:17:30 -0300

    add

---
+++ seçao3/exercicio-setinterval/assets/js/main.js
@@ -0,0 +1,18 @@
+// const clock = document.querySelector('.clock');
+// const start = document.querySelector('.start');
+// const stop = document.querySelector('.stop');
+// const reset = document.querySelector('.reset');
+
+// const hour = document.querySelector('#hour');
+// const min = document.querySelector('#min');
+// const sec = document.querySelector('#sec');
+
+function time() {
+    for (let sec = 0; sec < 3; sec++)
+    
+    return sec
+};
+
+setInterval(function(){
+    console.log(time());
+}, 1000);
