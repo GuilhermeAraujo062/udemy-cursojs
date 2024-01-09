4b76d79e8de9e7a39c7746818ee713a917b8fff7
Guilherme Araujo <99432207+GuilhermeAraujo062@users.noreply.github.com>
2024-01-09 14:36:31 -0300

    add

---
+++ seçao3/exercicio-tabela/assets/js/main.js
@@ -0,0 +1,7 @@
+const inputTask = document.querySelector('.input-task');
+const btnTask = document.querySelector('btn-task');
+const taskList = document.querySelector('task-list');
+
+btnTask.addEventListener('click', function(){
+    
+});
